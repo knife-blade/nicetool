13daffc28f571b76a1221f83b1a58d6041771b7b
knfeedge <2716552863@qq.com>
2024-01-14 22:41:40 +0800

    添加AOP工具类

---
+++ src/main/java/com/knife/util/util/AopUtil.java
@@ -0,0 +1,28 @@
+package com.knife.util.util;
+
+import org.springframework.aop.framework.Advised;
+
+public class AopUtil {
+    public static Class<?> getTargetClass(Object bean) {
+        Class<?> targetCls = bean.getClass();
+
+        // 如果类名包含此字符串则说明是代理类，记录其父类（真实目标类）
+        if (targetCls.getName().contains("$$EnhancerBySpringCGLIB$$")) {
+            targetCls = targetCls.getSuperclass();
+        }
+
+        return targetCls;
+    }
+
+    public static Object getTargetBean(Object bean) {
+        Object targetBean = bean;
+        if (targetBean instanceof Advised) {
+            try {
+                targetBean = ((Advised) targetBean).getTargetSource().getTarget();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return targetBean;
+    }
+}
