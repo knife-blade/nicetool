71d1b5c8db526655c1d44f4d7a7eb9141bfd66c3
knifeout <2716552863@qq.com>
2024-01-26 14:50:34 +0800

    添加必要注释

--- src/main/java/com/suchtool/betterutil/util/DateTimeUtil.java
+++ src/main/java/com/suchtool/betterutil/util/DateTimeUtil.java
@@ -9,36 +9,66 @@ import java.util.Date;
 
 public class DateTimeUtil {
     /**
-     * 格式化
+     * 格式化为字符串
      *
      * @param localDateTime 时间
-     * @param format        格式。见{@link DateTimeFormatConstant}
+     * @param format        格式。常用格式见{@link DateTimeFormatConstant}
      */
     public static String format(LocalDateTime localDateTime,
                                 String format) {
         return localDateTime.format(DateTimeFormatter.ofPattern(format));
     }
 
+    /**
+     * 格式化为字符串
+     *
+     * @param localDate 日期
+     * @param format    格式。常用格式见{@link DateTimeFormatConstant}
+     */
     public static String format(LocalDate localDate,
                                 String format) {
         return localDate.format(DateTimeFormatter.ofPattern(format));
     }
 
+    /**
+     * 格式化为字符串
+     *
+     * @param localTime 时间
+     * @param format    格式。常用格式见{@link DateTimeFormatConstant}
+     */
     public static String format(LocalTime localTime,
                                 String format) {
         return localTime.format(DateTimeFormatter.ofPattern(format));
     }
 
+    /**
+     * 解析为时间
+     * @param localDateTimeString 时间字符串
+     * @param format 格式。常用格式见{@link DateTimeFormatConstant}
+     * @return 时间
+     */
     public static LocalDateTime parseToLocalDateTime(String localDateTimeString,
                                                      String format) {
         return LocalDateTime.parse(localDateTimeString, DateTimeFormatter.ofPattern(format));
     }
 
+    /**
+     * 解析为日期
+     * @param localDateString 日期字符串
+     * @param format 格式。常用格式见{@link DateTimeFormatConstant}
+     * @return 日期
+     */
     public static LocalDate parseToLocalDate(String localDateString,
                                              String format) {
         return LocalDate.parse(localDateString, DateTimeFormatter.ofPattern(format));
     }
 
+    /**
+     * 解析为时间
+     * @param localTimeString 时间字符串
+     * @param format 格式。常用格式见{@link DateTimeFormatConstant}
+     * @return 时间
+     */
     public static LocalTime parseToLocalTime(String localTimeString,
                                              String format) {
         return LocalTime.parse(localTimeString, DateTimeFormatter.ofPattern(format));
