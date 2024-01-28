b9d02b25df478c616c59d300251227d0c1382160
knifeout <2716552863@qq.com>
2024-01-28 12:02:30 +0800

    完善README

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 # nice-util
 
 ## 介绍
-Java工具类。
+超好用的Java工具类。特点是：稳定、最大程度利用SpringBoot原生工具。
 
 ## 功能
-待完善
+1. ApplicationContextHolder：持有Spring的ApplicationContext，可以静态调用。例如：ApplicationContextHolder.getContext().getBean(Xxx.class);
+2. JsonUtil：Json工具。功能：JSON字符串转对象、对象转JSON字符串等。
+3. ThrowableUtil：异常工具。功能：获得异常的堆栈信息（字符串）等
+4. DateTimeUtil：日期时间工具。功能：格式化LocalDateTime、Date；解析时间字符串为Date；将LocalDateTime转为Date等
+5. BeanUtil：Bean工具。功能：对象浅拷贝、对象深拷贝等
+6. ValidateUtil：校验工具。功能：手动校验对象，等同于@Valid功能。
+7. IpUtil：IP工具。功能：获得调用方IP；获得客户端真实IP等；
+8. 其他功能
 
 ## 快速使用
 
 ### 1.引入依赖
+```xml
+<dependency>
+    <groupId>com.suchtool</groupId>
+    <artifactId>nice-util-spring-boot-starter</artifactId>
+    <version>{newest-version}</version>
+</dependency>
+```
 ### 2.使用
+```java
+String jsonString = JsonUtil.toJsonString(obj);
+```
 
--- src/main/java/com/suchtool/niceutil/util/PropertyUtil.java
+++ src/main/java/com/suchtool/niceutil/util/PropertyUtil.java
@@ -2,8 +2,13 @@ package com.suchtool.niceutil.util;
 
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.cglib.core.ReflectUtils;
+import org.springframework.util.StringUtils;
 
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -32,5 +37,56 @@ public class PropertyUtil {
  
         String[] result = new String[emptyNames.size()];
         return emptyNames.toArray(result);
+    }
+
+    /**
+     * 把对象中的 String 类型的空字段，转换为指定字符串
+     *
+     * @param <T> 待转化对象类型
+     * @param cls 待转化对象
+     * @param str 目标字符串
+     */
+    public static <T> void replaceBlankString(T cls, String str) {
+        Field[] fields = cls.getClass().getDeclaredFields();
+        if (fields == null || fields.length == 0) {
+            return;
+        }
+        for (Field field : fields) {
+            if ("String".equals(field.getType().getSimpleName())) {
+                field.setAccessible(true);
+                try {
+                    String value = (String) field.get(cls);
+                    if (!StringUtils.hasText(value)) {
+                        field.set(cls, str);
+                    }
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 判断是否所有字段都是null
+     * @param o 对象
+     * @return 所有的对象都是null
+     */
+    public static boolean allFieldAreNull(Object o) {
+        Class<?> aClass = o.getClass();
+
+        PropertyDescriptor[] beanProperties = ReflectUtils.getBeanProperties(aClass);
+        for (PropertyDescriptor beanProperty : beanProperties) {
+            Method readMethod = beanProperty.getReadMethod();
+            try {
+                Object value = readMethod.invoke(o);
+                if (value != null) {
+                    return false;
+                }
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return true;
     }
 }
