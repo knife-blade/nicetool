b8bd1820eba56ec5237676161002408630425bfc
knifeout <2716552863@qq.com>
2024-06-12 16:58:03 +0800

    1.1.0版本发布（参数组装支持忽略类）

--- src/main/java/com/suchtool/nicetool/util/reflect/MethodUtil.java
+++ src/main/java/com/suchtool/nicetool/util/reflect/MethodUtil.java
@@ -2,8 +2,10 @@ package com.suchtool.nicetool.util.reflect;
 
 import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
+import org.springframework.util.CollectionUtils;
 
 import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +15,7 @@ public class MethodUtil {
 
     /**
      * 解析方法详情
+     *
      * @param method 方法
      * @return 方法信息：类、方法名、参数类型
      */
@@ -26,21 +29,52 @@ public class MethodUtil {
 
     /**
      * 解析参数
+     *
      * @param method 方法
-     * @param args  参数
+     * @param args   参数
      * @return Map（key为参数名，value为参数值）
      */
     public static Map<String, Object> parseParam(Method method, Object[] args) {
+        return parseParam(method, args, null);
+    }
+
+    /**
+     * 解析参数
+     *
+     * @param method          方法
+     * @param args            参数
+     * @param ignoreClassList 忽略解析的类
+     * @return Map（key为参数名，value为参数值）
+     */
+    public static Map<String, Object> parseParam(Method method,
+                                                 Object[] args,
+                                                 Collection<Class<?>> ignoreClassList) {
         Map<String, Object> map = new HashMap<>();
 
         String[] parameterNames = NAME_DISCOVERER.getParameterNames(method);
         if (parameterNames != null
                 && parameterNames.length > 0) {
             for (int i = 0; i < parameterNames.length; i++) {
-                map.put(parameterNames[i], args[i]);
+                if (requireParse(ignoreClassList, args[i])) {
+                    map.put(parameterNames[i], args[i]);
+                }
             }
         }
 
         return map;
+    }
+
+    private static boolean requireParse(Collection<Class<?>> ignoreClassList,
+                                        Object arg) {
+        boolean requireParse = true;
+        if (!CollectionUtils.isEmpty(ignoreClassList)) {
+            for (Class<?> aClass : ignoreClassList) {
+                if (aClass.isInstance(arg)) {
+                    requireParse = false;
+                    break;
+                }
+            }
+        }
+        return requireParse;
     }
 }
