927761efb62e8fd603e9d0b0ad78724128658f99
knifeout <2716552863@qq.com>
2025-01-12 17:05:30 +0800

    1.1.12版本发布（完善CURL工具）

--- src/main/java/com/suchtool/nicetool/util/web/http/curl/vo/CurlVO.java
+++ src/main/java/com/suchtool/nicetool/util/web/http/curl/vo/CurlVO.java
@@ -2,6 +2,7 @@ package com.suchtool.nicetool.util.web.http.curl.vo;
 
 import com.suchtool.nicetool.util.web.http.curl.constant.CurlErrorCodeEnum;
 import lombok.Data;
+import org.springframework.util.MultiValueMap;
 
 @Data
 public class CurlVO {
@@ -11,7 +12,9 @@ public class CurlVO {
 
     private String errorMessage;
 
-    private Integer httpStatusCode;
+    private MultiValueMap<String, String> header;
 
     private String httpResponse;
+
+    private Integer httpStatusCode;
 }
