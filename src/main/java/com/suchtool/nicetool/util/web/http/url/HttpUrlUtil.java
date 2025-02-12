4cb014f0ca555cbf01a84ead36a45823a10e5226
knifeout <2716552863@qq.com>
2025-02-12 09:37:48 +0800

    1.1.21版本发布（添加URL参数编码功能）

--- src/main/java/com/suchtool/nicetool/util/web/http/url/HttpUrlUtil.java
+++ src/main/java/com/suchtool/nicetool/util/web/http/url/HttpUrlUtil.java
@@ -5,12 +5,10 @@ import org.springframework.util.CollectionUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class HttpUrlUtil {
     /**
@@ -20,7 +18,7 @@ public class HttpUrlUtil {
      * @param addScheme 是否自动添加协议头
      * @return 拼接好的URL
      */
-    public static String joinUrl(List<String> fragments, boolean addScheme) {
+    public static String joinUrl(Collection<String> fragments, boolean addScheme) {
         StringBuilder result = new StringBuilder();
 
         for (String fragment : fragments) {
@@ -56,12 +54,13 @@ public class HttpUrlUtil {
      * @param fragments URL片段
      * @return 拼接好的URL
      */
-    public static String joinUrl(List<String> fragments) {
+    public static String joinUrl(Collection<String> fragments) {
         return joinUrl(fragments, true);
     }
 
     /**
      * 转为参数字符串
+     *
      * @param paramMap 参数Map
      * @return 参数字符串
      */
@@ -93,12 +92,13 @@ public class HttpUrlUtil {
 
     /**
      * 转为参数字符串
-     * @param paramList 参数列表
+     *
+     * @param params 参数列表
      * @return 参数字符串
      */
-    public static String toParamString(List<Object> paramList) {
+    public static String toParamString(Collection<Object> params) {
         Map<Object, Object> map = new HashMap<>();
-        for (Object obj : paramList) {
+        for (Object obj : params) {
             Class<?> cls = obj.getClass();
             fillParamMap(map, cls, obj);
         }
@@ -128,23 +128,90 @@ public class HttpUrlUtil {
     /**
      * 通过对象构造带参数的URL
      *
-     * @param baseUrl   url前缀
-     * @param paramList 对象列表（用此注解指定字段名：{@link UrlParamProperty}）
+     * @param baseUrl url前缀
+     * @param params  对象列表（用此注解指定字段名：{@link UrlParamProperty}）
      * @return 带参数的url
      */
-    public static String buildUrl(String baseUrl, List<Object> paramList) {
+    public static String buildUrl(String baseUrl, Collection<Object> params) {
         StringBuilder stringBuilder = new StringBuilder(baseUrl);
 
-        if (CollectionUtils.isEmpty(paramList)) {
+        if (CollectionUtils.isEmpty(params)) {
             return stringBuilder.toString();
         }
 
         stringBuilder.append("?");
-        String paramString = toParamString(paramList);
+        String paramString = toParamString(params);
 
         stringBuilder.append(paramString);
 
         return stringBuilder.toString();
+    }
+
+    /**
+     * 对url指定的参数进行编码
+     *
+     * @param url    要编码的url
+     * @param params 要编码的url参数
+     * @return 编码后的url
+     */
+    public static String encodeUrlParam(String url,
+                                        Collection<String> params) {
+        try {
+            return doEncodeUrlParam(url, params);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String doEncodeUrlParam(String url,
+                                           Collection<String> params) throws Exception {
+        URL originalUrl = new URL(url);
+        String query = originalUrl.getQuery();
+        Map<String, String> paramMap = splitQuery(query);
+
+        for (Map.Entry<String, String> entry : paramMap.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+
+            if (params.contains(key)) {
+                String encodedValue = URLEncoder.encode(value, "UTF-8");
+                paramMap.put(key, encodedValue);
+            }
+        }
+
+        return buildUrl(originalUrl, paramMap);
+    }
+
+    private static Map<String, String> splitQuery(String query) {
+        Map<String, String> queryParamMap = new LinkedHashMap<>();
+        if (query == null || query.isEmpty()) {
+            return queryParamMap;
+        }
+
+        String[] keyValuePairs = query.split("&");
+        for (String pair : keyValuePairs) {
+            int idx = pair.indexOf("=");
+            queryParamMap.put(pair.substring(0, idx), pair.substring(idx + 1));
+        }
+
+        return queryParamMap;
+    }
+
+    private static String buildUrl(URL originUrl,
+                                   Map<String, String> paramMap) throws Exception {
+        StringBuilder newQuery = new StringBuilder();
+        for (Map.Entry<String, String> param : paramMap.entrySet()) {
+            if (newQuery.length() > 0) {
+                newQuery.append("&");
+            }
+            newQuery.append(param.getKey()).append("=").append(param.getValue());
+        }
+
+        return new URL(
+                originUrl.getProtocol(),
+                originUrl.getHost(),
+                originUrl.getPort(),
+                originUrl.getPath() + "?" + newQuery).toString();
     }
 
     private static void fillParamMap(Map<Object, Object> paramMap,
