0daaaf0aebdd63fe95c0747743be177938d50094
knifeout <2716552863@qq.com>
2025-10-06 20:33:53 +0800

    1.1.29版本发布（时间常量添加毫秒）

--- src/main/java/com/suchtool/nicetool/util/lib/datetime/constant/DateTimeFormatConstant.java
+++ src/main/java/com/suchtool/nicetool/util/lib/datetime/constant/DateTimeFormatConstant.java
@@ -1,6 +1,8 @@
 package com.suchtool.nicetool.util.lib.datetime.constant;
  
 public interface DateTimeFormatConstant {
+    String DATE_TIME_FORMAT_FULL = "yyyy-MM-dd HH:mm:ss.SSS";
+
     String DATE_TIME_FORMAT_NORMAL = "yyyy-MM-dd HH:mm:ss";
  
     String DATE_FORMAT_NORMAL = "yyyy-MM-dd";
