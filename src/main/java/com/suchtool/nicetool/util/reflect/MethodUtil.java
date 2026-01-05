7a343a075c3a263a341ca76e4dcf2cca919bd0ce
knifeout <2716552863@qq.com>
2026-01-05 22:50:24 +0800

    1.1.34版本发布（添加方法工具）

--- src/main/java/com/suchtool/nicetool/util/reflect/MethodUtil.java
+++ src/main/java/com/suchtool/nicetool/util/reflect/MethodUtil.java
@@ -2,6 +2,7 @@ package com.suchtool.nicetool.util.reflect;
 
 import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
+import org.springframework.util.ClassUtils;
 import org.springframework.util.CollectionUtils;
 
 import java.lang.reflect.Method;
@@ -64,6 +65,33 @@ public class MethodUtil {
         return map;
     }
 
+    /**
+     * 解析参数
+     *
+     * @param method              方法
+     * @param args                参数
+     * @param ignoreClassNameList 忽略解析的类名
+     * @return Map（key为参数名，value为参数值）
+     */
+    public static Map<String, Object> parseParamForClassName(
+            Method method,
+            Object[] args,
+            Collection<String> ignoreClassNameList) {
+        Map<String, Object> map = new HashMap<>();
+
+        String[] parameterNames = NAME_DISCOVERER.getParameterNames(method);
+        if (parameterNames != null
+                && parameterNames.length > 0) {
+            for (int i = 0; i < parameterNames.length; i++) {
+                if (requireParseForClassName(ignoreClassNameList, args[i])) {
+                    map.put(parameterNames[i], args[i]);
+                }
+            }
+        }
+
+        return map;
+    }
+
     public static boolean requireParse(Collection<Class<?>> ignoreClassList,
                                        Object arg) {
         boolean requireParse = true;
@@ -75,6 +103,34 @@ public class MethodUtil {
                 }
             }
         }
+        return requireParse;
+    }
+
+    public static boolean requireParseForClassName(Collection<String> ignoreClassNames, Object arg) {
+        boolean requireParse = true;
+
+        if (arg == null) {
+            return false; // 如果参数为 null，直接返回 false
+        }
+
+        if (ignoreClassNames != null && !ignoreClassNames.isEmpty()) {
+            for (String className : ignoreClassNames) {
+                try {
+                    if (ClassUtils.isPresent(className, MethodUtil.class.getClassLoader())) {
+                        Class<?> aClass = Class.forName(className);
+                        if (aClass.isInstance(arg)) {
+                            requireParse = false;
+                            break;
+                        }
+                    }
+
+                } catch (Throwable e) {
+                    // 处理找不到类的异常（可以选择记录日志或抛出异常）
+                    System.err.println("Class not found: " + className);
+                }
+            }
+        }
+
         return requireParse;
     }
 }
